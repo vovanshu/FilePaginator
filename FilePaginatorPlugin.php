d2658454de464293dab3df41fdb78d3d88b6fd4d
Ken <ken.albers@gmail.com>
2019-04-29 13:52:03 -0400

    Updated to use filter

--- FilePaginatorPlugin.php
+++ FilePaginatorPlugin.php
@@ -12,7 +12,16 @@
 
 class FilePaginatorPlugin extends Omeka_Plugin_AbstractPlugin
 {
-    protected $_hooks = array('public_head');
+    protected $_hooks = array(
+        'public_head',
+        'config', 
+        'config_form',
+        );
+
+    protected $_filters = array(
+        'files_for_item',
+        'file_markup'
+    );
 
     public function hookPublicHead($args)
     {   
@@ -20,6 +29,32 @@ class FilePaginatorPlugin extends Omeka_Plugin_AbstractPlugin
         queue_js_file('filePagination');
         queue_css_file('simplePagination');
         
+    }
+
+    public function hookConfig($args){
+        $post = $args['post'];
+        set_option('file_paginator_theme',$post['theme']);
+        set_option('file_paginator_links'),$post['links'];
+    }
+
+    public function hookConfigForm() {
+        include('config_form.php');
+    }
+
+    public function filterFilesForItem($html) {
+        return '<div id="pagination"></div>'.$html;
+    }
+
+    public function filterFileMarkup($html,$args) {
+        $file = $args['file'];
+        $options =  $args['options'];
+        if (empty($options['filesForItem'])) {
+            return $html;
+        }
+        return '<div class="single-file">'
+            . $html
+            . all_element_texts($file, array('show_element_set_headings' => false))
+            . '</div>';
     }
 
     public static function paginateFiles() {
